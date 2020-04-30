233494f668c01e2d18f562557542177629cf9b65
rubensojunior <rubensojunior6@gmail.com>
2020-04-30 14:06:29 -0300

    creating main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 import GlobalStyle from './styles/global'
 
+import Main from './pages/Main'
+
 const App: React.FC = () => (
   <>
+    <Main />
     <GlobalStyle />
   </>
 )
